5316ec3c6149f7172e68e766b63ebdc0e8f8c868
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-10-18 16:28:35 +0200

    Exercicios do feedback

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import React from "react";
 import CardVideo from "./Componentes/CardVideo";
 import "./styles.css";
 
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    width: 100%;
+    padding: 0;
+    margin: 0;
+  }
+`
+const HeaderStyle = styled.header`
+  background-color: orange;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+  height: 10%;
+`
+const MainStyle = styled.main`
+  min-height: 80%;
+  display: flex;
+`
+
+const NavStyle = styled.nav`
+  flex-basis: 200px;
+  border-right-style: solid;
+  border-right-width: thin;
+`
+
 export default function App() {
   const card1 = {
     titulo: "Título do vídeo",
@@ -10,15 +38,17 @@ export default function App() {
   };
 
   return (
+    <>
+    <GlobalStyle/>
     <div>
       <div className="tela-inteira">
-        <header>
+        <HeaderStyle>
           <h1>LabeTube</h1>
           <input type="text" placeholder="Busca" id="campoDeBusca" />
-        </header>
+        </HeaderStyle>
 
-        <main>
-          <nav className="menu-vertical">
+        <MainStyle>
+          <NavStyle className="menu-vertical">
             <ul>
               <li className="botoes-meunu-vertical">Início</li>
               <li className="botoes-meunu-vertical">Em alta</li>
@@ -27,7 +57,7 @@ export default function App() {
               <li className="botoes-meunu-vertical">Originais</li>
               <li className="botoes-meunu-vertical">Histórico</li>
             </ul>
-          </nav>
+          </NavStyle>
 
           <section className="painel-de-videos">
             <CardVideo
@@ -36,12 +66,13 @@ export default function App() {
               textoAlternativo={card1.textoAlternativo}
             />
           </section>
-        </main>
+        </MainStyle>
 
         <footer>
           <h4>Oi! Eu moro no footer!</h4>
         </footer>
       </div>
     </div>
+    </>
   );
 }
--- src/Componentes/CardVideo.js
+++ src/Componentes/CardVideo.js
@@ -1,12 +1,16 @@
+import {DivStyle} from './style'
+import {ImgStyle} from './style'
+
 function CardVideo(props) {
   function reproduzVideo(props) {
     alert("O vídeo está sendo reproduzido");
   }
   return (
-    <div className="box-pagina-principal" onClick={reproduzVideo}>
-      <img src={props.image1} alt={props.textoAlternativo} />
+    <DivStyle onClick={reproduzVideo}>
+      {/* className="box-pagina-principal"  */}
+      <ImgStyle src={props.image1} alt={props.textoAlternativo} />
       <h4>{props.titulo}</h4>
-    </div>
+    </DivStyle>
   );
 }
 
